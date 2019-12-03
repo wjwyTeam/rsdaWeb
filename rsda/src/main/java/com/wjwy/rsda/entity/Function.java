704c9af80f78be3d678ae862382438a56a7ed66b
zhangguangrui <505923239@qq.com>
2019-12-03 15:22:44 +0800

    function初始化class

---
+++ rsda/src/main/java/com/wjwy/rsda/entity/Function.java
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @version: v0.0.1
+ * @Author: zgr
+ * @Date: 2019-12-03 15:08:01
+ * @LastEditors: zgr
+ * @LastEditTime: 2019-12-03 15:14:17
+ */
+package com.wjwy.rsda.entity;
+public class Function{
+    
+}
